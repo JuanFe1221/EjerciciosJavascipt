81c19ab299ed3a7c0a0fa4207899f7e94276cc65
Camilo <benitocamelo0002@gmail.com>
2022-02-07 16:14:28 -0500

    cambios

--- js/Ejercicio_1.js
+++ js/Ejercicio_1.js
@@ -4,3 +4,20 @@ console.log("Hola mundo")
 /* Ejercicio 2 */
 let nombreAprendiz = "Juan"
 console.log("Bienvenido, sr(a)  " + nombreAprendiz)
+/* Ejercicio 3 */
+
+let precioProducto = 20000;
+let impuesto = 0.10;
+let domicilio = 3000;
+
+let precioInicial = precioProducto;
+    console.log("El precio inicial es de:" + precioInicial);
+
+    let subPrecio = precioProducto +  (precioProducto * impuesto);
+    console.log("El sub precio tomando en cuenta el impuesto es de: " + subPrecio);
+
+    let precioTotal = subPrecio +  domicilio;
+    console.log("El precio total del servicio incluido el impuesto y el domicilio  es de:" + precioTotal)
+
+ 
+
