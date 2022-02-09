c7d1d5070cb97b1eb22cced32c792c1ecd94ef2b
unknown <SENA@PITAPRTNNFSD274.FormacionSena.RED>
2022-02-09 13:46:00 -0500

    cambios

---
+++ js/Ejercicio_2.js
@@ -0,0 +1,13 @@
+"use strict"
+ 
+
+
+let aprendiz1 = new Array();
+aprendiz1[1] = "Juan Felipe";
+aprendiz1[2] = "Deigo Antonio";
+aprendiz1[3] = "Kevin ijaji";
+aprendiz1[4] = "Jhoan Sebastian";
+
+ for(let f = 0; f <= 6 -1; f++){
+    console.log(aprendiz1[f]);
+ }
